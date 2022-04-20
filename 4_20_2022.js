f01a17058cc12baea7bc97315d99b2cd62df769b
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-20 18:20:31 +0900

    .

---
+++ 4_20_2022.js
@@ -0,0 +1,8 @@
+
+// Language: javascript
+// Date: 4/20/2022
+// Path: 4_20_2022.js
+// Time: 30 min for researching + server migration
+// Description: migrated db server to be located closer to the free vercel tier server (washington dc IAD1)
+//https://vercel.com/support/articles/choosing-deployment-regions
+//https://github.com/supabase/supabase/discussions/4815
