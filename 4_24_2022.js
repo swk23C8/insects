40be24b60387ea521967a165dbbf41a6002f56f2
swk23c8 <35516289+swk23C8@users.noreply.github.com>
2022-04-24 21:21:58 +0900

    .

---
+++ 4_24_2022.js
@@ -0,0 +1,11 @@
+
+// Language: javascript
+// Date: 4/24/2022
+// Path: 4_24_2022.js
+// Time: 10min
+// Description: learning React.Fragment
+
+// basically the same as empty tags but with key attribute
+
+// https://medium.com/fasal-engineering/what-are-react-fragments-or-the-react-empty-tags-190253582905
+// https://reactjs.org/docs/fragments.html
